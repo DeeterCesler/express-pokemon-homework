70d33adb84264dabbaf0b7d3941ceb96e2033c80
Deeter <deeter.cesler@gmail.com>
2018-10-09 18:06:22 -0600

    now shows pokemon details

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const Pokemon = require("./models/pokemon.js");
 app.use(express.static(__dirname + '/views'));
 
 app.get("/pokemon", (req, res) => {
-    // res.send(Pokemon);
     res.render("index.ejs", {
         pokemonList: Pokemon
     })
 })
 
+app.get("/pokemon/:id", (req, res) => {
+    res.render("show.ejs", {
+        pokemon: Pokemon[req.params.id]
+    })
+})
 
 app.listen(3000, () => {
     console.log("server is running via port 3000")
